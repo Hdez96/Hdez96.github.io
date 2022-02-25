f98a40067cfc730fe5b916c983777f5d3c5c3daf
Hdez96 <chernandezz1500@alumno.ipn.mx>
2022-02-25 16:07:23 -0600

    Update script-Carrousel.js

--- scripts/script-Carrousel.js
+++ scripts/script-Carrousel.js
@@ -8,32 +8,32 @@
  let cambioImagen = (param) => {
      if(param === 'izq' && actualImg === 1){
             actualImg = 4
-            document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".JPG')"
+            document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".jpg')"
         }else if(param === 'der' && actualImg === 5){
             actualImg = 1
-            document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".JPG')"
+            document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".jpg')"
         }else if(param === 'izq' && actualImg != 1){
             actualImg--
-            document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".JPG')"
+            document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".jpg')"
         }else if(param === 'der' && actualImg != 5){
             actualImg++
-            document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".JPG')"
+            document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".jpg')"
         }  
 }
 
 let cambioImagenAuto = () => {
     if(actualImg === 1){
            actualImg = 5
-           document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".JPG')"
+           document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".jpg')"
        }else if(actualImg === 5){
            actualImg = 1
-           document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".JPG')"
+           document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".jpg')"
        }else if(actualImg != 1){
            actualImg--
-           document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".JPG')"
+           document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".jpg')"
        }else if(actualImg != 5){
            actualImg++
-           document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".JPG')"
+           document.getElementById('cajaC').style.backgroundImage="url('https://raw.githubusercontent.com/Hdez96/hdez96.github.io/main/img/Carrusel/img" + actualImg + ".jpg')"
        }  
 }
 
